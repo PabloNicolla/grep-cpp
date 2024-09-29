c9878c8e48090014647e17e8b33a734245169b26
Pablo Nicola <pablofnic@gmail.com>
2024-09-29 18:14:12 -0400

    regex custom logic

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,63 @@
 #include <algorithm>
 #include <functional>
 
-constexpr bool isW(const char c) {
+constexpr bool isAnyMetaCharacter(const char c) {
+    return (
+            (c == '^') ||
+            (c == '$') ||
+
+            (c == '[') ||
+            (c == ']') ||
+            (c == '{') ||
+            (c == '}') ||
+            (c == '(') ||
+            (c == ')') ||
+
+            (c == '+') ||
+            (c == '*') ||
+            (c == '?') ||
+
+            (c == '.') ||
+
+            (c == '\\') ||
+
+            (c == '|')
+    );
+}
+
+constexpr bool isQuantifier(const char c) {
+    return (
+            (c == '+') ||
+            (c == '*') ||
+            (c == '?')
+    );
+}
+
+constexpr bool isCharacterClass(const char c) {
+    return (
+            (c == 'w') ||
+            (c == 'W') ||
+            (c == 's') ||
+            (c == 'S') ||
+            (c == 'd') ||
+            (c == 'D') ||
+            (c == 'w') ||
+            (c == 'W') ||
+            (c == 'x') ||
+            (c == 'O') ||
+            (c == 'c')
+    );
+}
+
+
+
+
+
+
+
+
+
+constexpr bool is_w(const char c) {
     return (
             (c >= 'a' && c <= 'z') ||
             (c >= 'Z' && c <= 'Z') ||
@@ -12,6 +68,116 @@ constexpr bool isW(const char c) {
             (c == '_')
     );
 }
+constexpr bool is_W(const char c) {
+    return !is_w(c);
+}
+
+constexpr bool is_d(const char c) {
+    return isdigit(c);
+}
+constexpr bool is_D(const char c) {
+    return !is_d(c);
+}
+
+
+
+
+
+
+
+std::function<bool(const char)> characterClassSelector(const char c) {
+    switch (c)
+    {
+    case 'w':
+        return is_w;
+        break;
+    case 'W':
+        return is_W;
+        break;
+    case 'd':
+        return is_d;
+        break;
+    case 'D':
+        return is_D;
+        break;
+    
+    default:
+        throw std::invalid_argument("Character Class must receive valid character");
+        break;
+    }
+}
+
+
+
+std::function<bool(const char)> plainCharacterMatcherBuilder(const std::vector<const char>& plainChars) {
+    return [&plainChars](const char c) -> bool {
+        for (const auto plainChar : plainChars) {
+            if (plainChar == c) {
+                return true;
+            }
+        }
+        return false;
+    };
+}
+
+
+
+
+
+
+
+
+
+
+bool matcherControl(const std::vector<std::function<bool(const char)>>& matchers, const char c) {
+    for (const auto& matcher : matchers) {
+        if (matcher(c)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool matcherControlSetup(const std::string& simple_sub_pattern, const char c) {
+    std::vector<std::function<bool(const char)>> matchers{};
+    std::vector<const char> plainChars{};
+
+    auto it = simple_sub_pattern.begin();
+    while (it != simple_sub_pattern.end()) {
+        // Early exit if sub_pattern contains `.`
+        if (*it == '.') {
+            return true;
+        }
+        // handle scape character
+        else if (*it == '\\') {
+            if (it + 1 == simple_sub_pattern.end()) {
+                throw std::invalid_argument("Error: Dangling backslash.");
+            }
+            else if (isCharacterClass(*(it + 1))) {
+                matchers.push_back(characterClassSelector(*(it + 1)));
+            }
+            else {
+                plainChars.push_back(*(it + 1));
+            }
+            it += 2;
+        }
+        // plain character
+        else {
+            plainChars.push_back(*it);
+            ++it;
+        }
+    }
+
+    matchers.push_back(plainCharacterMatcherBuilder(plainChars));
+    return matcherControl(matchers, c);
+}
+
+
+
+
+
+
+
 
 std::function<bool(const char)> handlePositiveGroup(const std::string& groupPattern) {
     return [groupPattern](const char c) -> bool {
@@ -24,21 +190,10 @@ std::function<bool(const char)> handlePositiveGroup(const std::string& groupPatt
     };
 }
 
-std::function<bool(const char)> handleNegativeGroup(const std::string& groupPattern) {
-    return [groupPattern](const char c) -> bool {
-        for (auto it = groupPattern.begin() + 1, end = groupPattern.end() - 1; it != end; ++it) {
-            if (*it == c) {
-                return false;
-            }
-        }
-        return true;
-    };
-}
-
 std::function<bool(const char)> handleGroups(const std::string &groupPattern) {
-    if (groupPattern.size() < 2) {
-        throw std::invalid_argument("Group Pattern too short");
-    }
+    // if (groupPattern.size() < 2) {
+    //     throw std::invalid_argument("Group Pattern too short");
+    // }
 
     if (groupPattern[1] != '^') return handlePositiveGroup(groupPattern);
     else if (groupPattern[1] == '^') return handleNegativeGroup(groupPattern);
@@ -112,3 +267,6 @@ int main(int argc, char *argv[])
         return 1;
     }
 }
+
+
+void match
